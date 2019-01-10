88ff9447a317c14ec77d9d1c4438c04c735fa166
Aarkan1 <johanwiren1@gmail.com>
2019-01-10 15:48:39 +0100

    removed authentication

--- src/main.js
+++ src/main.js
@@ -9,12 +9,7 @@ import 'firebase/database';
 
 // connect to firebase database
 firebase.initializeApp({
-    apiKey: "AIzaSyBRrkp5xlsol3iPFmMoUg_c6tz9bNmoag0",
-    authDomain: "arcquest-85bc5.firebaseapp.com",
-    databaseURL: "https://arcquest-85bc5.firebaseio.com",
-    projectId: "arcquest-85bc5",
-    storageBucket: "arcquest-85bc5.appspot.com",
-    messagingSenderId: "727232800872"
+    
 });
 let dbRef = firebase.database();
 // export the ref to the todos list
